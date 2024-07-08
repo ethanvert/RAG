8837f67b97086ce1ef95a12122fa31cca4ffa211
ethanvert <ethan.vertal@gmail.com>
2024-07-08 14:07:13 -0400

    begun data collection process

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from backend import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from backend import app, db
+from backend.dataclass import User, Post
 
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
